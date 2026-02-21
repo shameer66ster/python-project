914bc0bdff436336b91e99478684ebd1199fe378
Shameer <shameer66ster@gmail.com>
2026-02-21 16:09:25 +0530

    feat: very basics

---
+++ lecture-one/variables.py
@@ -0,0 +1,12 @@
+#basics very basic
+cost = 10
+tax_percent = .25
+tax = cost * tax_percent
+price = cost + tax
+print(price)
+
+username = "coding with python"
+first_name = "shameer"
+print(username + " " + first_name)
+
+first_number = 10
